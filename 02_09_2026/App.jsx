ab9b1bc9dd5e233d9b8dabbece2b8c2b499dfc45
Hemanth4596 <hemanthsrisai4596@gmail.com>
2026-09-02 12:22:41 +0530

    Adding app

---
+++ 02_09_2026/App.jsx
@@ -0,0 +1,11 @@
+import RegistrationForm from "./Registrationform";
+
+function App() {
+    return (
+        <div>
+            <RegistrationForm />
+        </div>
+    );
+}
+
+export default App;
---
+++ 02_09_2026/Registrationform.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import "./registration.css"; 
+function RegistrationForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      name: name,
+      email: email,
+      phone: phone
+    };
+
+    // Store data
+    localStorage.setItem("userData", JSON.stringify(userData));
+
+    // Check in browser console
+    console.log("Saved Data:", userData);
+
+    // Go to display.html (use correct path)
+    window.location.href = "/display.html";
+  };
+
+  return (
+    <div className="container">
+      <h2>Registration Form</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter your name"
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Phone:</label>
+          <input
+            type="tel"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            placeholder="Enter your phone number"
+            required
+          />
+        </div>
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+    
+  );
+}
+
+export default RegistrationForm;
